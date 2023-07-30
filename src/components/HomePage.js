13a40c056d7bef4577159b38411a23e45a5aa540
Claudia  P. R. Soto <97201255+ClaudiaRojasSoto@users.noreply.github.com>
2023-07-30 18:28:02 -0400

    Delete the images folder from the public folder and add the folder image to the source folder with the images of the continents and the flags of america and europe

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,7 +1,30 @@
 import React from 'react';
+import '../styles/HomePage.css';
+
+import continent1 from '../images/continents/afrika.png';
+import continent2 from '../images/continents/amerika.png';
+import continent3 from '../images/continents/asien.png';
+import continent4 from '../images/continents/australien.png';
+import continent5 from '../images/continents/europa.png';
+import continent6 from '../images/continents/ozeanien.png';
 
 const HomePage = () => (
-  <h1>Home Page</h1>
+  <div className="home-page">
+    <div className="continent-grid">
+      <div className="continent-row">
+        <img src={continent1} alt="Continent 1" />
+        <img src={continent2} alt="Continent 2" />
+      </div>
+      <div className="continent-row">
+        <img src={continent3} alt="Continent 3" />
+        <img src={continent4} alt="Continent 4" />
+      </div>
+      <div className="continent-row">
+        <img src={continent5} alt="Continent 5" />
+        <img src={continent6} alt="Continent 6" />
+      </div>
+    </div>
+  </div>
 );
 
 export default HomePage;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,6 +4,7 @@ import { NavLink } from 'react-router-dom';
 const Navbar = () => (
   <nav>
     <NavLink to="/" exact activeclassName="active">Home</NavLink>
+    {' '}
     <NavLink to="/details/1" activeClassName="active">Details</NavLink>
   </nav>
 );
