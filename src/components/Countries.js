33a9e88921584f3cb0073b2c6d3942755cdbca1c
Claudia  P. R. Soto <97201255+ClaudiaRojasSoto@users.noreply.github.com>
2023-07-30 23:25:48 -0400

    Upload the src/components/Countries.js file to use the right continent image in the countries component

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -1,5 +1,35 @@
+import React from 'react';
 import { useParams } from 'react-router-dom';
 import flagsData from '../flagData';
+import '../styles/Countries.css'; // Importa el archivo CSS para el componente Countries
+
+// Importa las imágenes de los continentes
+import continent1 from '../imagesContinents/continents/africa.png';
+import continent2 from '../imagesContinents/continents/america.png';
+import continent3 from '../imagesContinents/continents/asia.png';
+import continent4 from '../imagesContinents/continents/australia.png';
+import continent5 from '../imagesContinents/continents/europa.png';
+import continent6 from '../imagesContinents/continents/oceania.png';
+
+// Función para obtener la imagen del continente
+function getContinentImage(continent) {
+  switch (continent) {
+    case 'africa':
+      return continent1;
+    case 'america':
+      return continent2;
+    case 'asia':
+      return continent3;
+    case 'australia':
+      return continent4;
+    case 'europa':
+      return continent5;
+    case 'oceania':
+      return continent6;
+    default:
+      return null;
+  }
+}
 
 const Countries = () => {
   const { continent } = useParams();
@@ -15,13 +45,19 @@ const Countries = () => {
 
   return (
     <div>
-      <h1>{`Países de ${continent}`}</h1>
-      {countriesOfContinent.map((country) => (
-        <div key={country.name}>
-          <img src={country.imagePath} alt={country.name} />
-          <p>{country.name}</p>
-        </div>
-      ))}
+      {/* Agrega la imagen y el nombre del continente aquí */}
+      <img src={getContinentImage(continent)} alt={continent} />
+      <h1>{continent}</h1>
+      <div className="countries-grid">
+        {/* Resto del código sigue igual */}
+        {countriesOfContinent.map((country) => (
+          <div key={country.name} className="country-item">
+            {/* Resto del código sigue igual */}
+            <img src={country.imagePath} alt={country.name} />
+            <p>{country.name}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
