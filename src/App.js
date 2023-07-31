5eca2fde978cf56d3a94b48778a148086a859546
Claudia  P. R. Soto <97201255+ClaudiaRojasSoto@users.noreply.github.com>
2023-07-31 19:05:44 -0400

    Add the route to details page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import store from './redux/store';
 import HomePage from './components/HomePage';
 import Navbar from './components/Navbar';
 import Countries from './components/Countries';
+import DetailsPage from './components/DetailsPage'; // asegúrate de importar DetailsPage
 
 function App() {
   return (
@@ -14,6 +15,8 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/countries/:continent" element={<Countries />} />
+          <Route path="/country/:name" element={<DetailsPage />} />
+          <Route path="/details/1" element={<DetailsPage />} />
         </Routes>
       </Router>
     </Provider>
