2d044b5ce40076da0cd906c443c6d26033fecaba
Claudia  P. R. Soto <97201255+ClaudiaRojasSoto@users.noreply.github.com>
2023-07-31 12:25:08 -0400

    Add the provider store wraping the router on app.js file

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './redux/store';
 import HomePage from './components/HomePage';
 import Navbar from './components/Navbar';
 import Countries from './components/Countries';
 
 function App() {
   return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/countries/:continent" element={<Countries />} />
-      </Routes>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/countries/:continent" element={<Countries />} />
+        </Routes>
+      </Router>
+    </Provider>
   );
 }
 
