0e8a93f801b9652fc10acf1959fcfd070c76e5b3
Claudia  P. R. Soto <97201255+ClaudiaRojasSoto@users.noreply.github.com>
2023-07-31 19:09:43 -0400

    Upload actions file to fix errors

--- src/redux/reducers/countriesReducer.js
+++ src/redux/reducers/countriesReducer.js
@@ -1,5 +1,6 @@
 const initialState = {
   countries: [],
+  countryData: null,
 };
 
 const countriesReducer = (state = initialState, action) => {
@@ -8,6 +9,11 @@ const countriesReducer = (state = initialState, action) => {
       return {
         ...state,
         countries: action.payload,
+      };
+    case 'UPDATE_COUNTRY_DATA':
+      return {
+        ...state,
+        countryData: action.payload,
       };
     default:
       return state;
