153b8fe3572321ea611d3a057515a3fc307e4d8f
Claudia  P. R. Soto <97201255+ClaudiaRojasSoto@users.noreply.github.com>
2023-07-30 23:35:38 -0400

    Add the getContinentImage funtion and the capitalizeFirstLetter to show the continents on the flags page and to show the name with the first letter on capital in the same page

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import flagsData from '../flagData';
-import '../styles/Countries.css'; // Importa el archivo CSS para el componente Countries
+import '../styles/Countries.css';
 
-// Importa las imágenes de los continentes
 import continent1 from '../imagesContinents/continents/africa.png';
 import continent2 from '../imagesContinents/continents/america.png';
 import continent3 from '../imagesContinents/continents/asia.png';
@@ -11,7 +10,6 @@ import continent4 from '../imagesContinents/continents/australia.png';
 import continent5 from '../imagesContinents/continents/europa.png';
 import continent6 from '../imagesContinents/continents/oceania.png';
 
-// Función para obtener la imagen del continente
 function getContinentImage(continent) {
   switch (continent) {
     case 'africa':
@@ -43,16 +41,17 @@ const Countries = () => {
     );
   }
 
+  const capitalizeFirstLetter = (
+    string,
+  ) => string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
+
   return (
     <div>
-      {/* Agrega la imagen y el nombre del continente aquí */}
       <img src={getContinentImage(continent)} alt={continent} />
-      <h1>{continent}</h1>
+      <h1>{capitalizeFirstLetter(continent)}</h1>
       <div className="countries-grid">
-        {/* Resto del código sigue igual */}
         {countriesOfContinent.map((country) => (
           <div key={country.name} className="country-item">
-            {/* Resto del código sigue igual */}
             <img src={country.imagePath} alt={country.name} />
             <p>{country.name}</p>
           </div>
