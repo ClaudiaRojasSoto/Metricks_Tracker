a1b9ac76b5e37bff9f26e4ba86b2938d2adf46b8
Claudia  P. R. Soto <97201255+ClaudiaRojasSoto@users.noreply.github.com>
2023-08-01 00:50:42 -0400

    change the logic to implement the api call on the reducer actions

--- src/redux/actions/countriesActions.js
+++ src/redux/actions/countriesActions.js
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 const updateCountries = (countries) => ({
   type: 'UPDATE_COUNTRIES',
   payload: countries,
@@ -8,4 +10,17 @@ const updateCountryData = (countryData) => ({
   payload: countryData,
 });
 
-export { updateCountries, updateCountryData };
+const fetchCountriesByContinent = (continent) => async (dispatch) => {
+  try {
+    const response = await axios.get(`https://restcountries.com/v3.1/region/${continent}`);
+    if (response.data && response.data.length > 0) {
+      dispatch(updateCountries(response.data));
+    } else {
+      console.log('No se devolvieron datos válidos del continente en la API');
+    }
+  } catch (error) {
+    console.log('Error al obtener los datos del continente:', error);
+  }
+};
+
+export { updateCountries, updateCountryData, fetchCountriesByContinent };
