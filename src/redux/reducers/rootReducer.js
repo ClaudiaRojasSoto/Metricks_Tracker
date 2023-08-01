0aea14e0e36a4ff364ac211e334113a4c9a31710
Claudia  P. R. Soto <97201255+ClaudiaRojasSoto@users.noreply.github.com>
2023-08-01 00:49:09 -0400

    Add the countriesReducer to the root

--- src/redux/reducers/rootReducer.js
+++ src/redux/reducers/rootReducer.js
@@ -3,7 +3,6 @@ import countriesReducer from './countriesReducer';
 
 const rootReducer = combineReducers({
   countries: countriesReducer,
-  // Otros reducers si los tienes.
 });
 
 export default rootReducer;
